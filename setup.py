afe3211e56d8f22effff8f1b46a11ab1a5f5e8b5
Daniel Ryan <dryan@dryan.com>
2012-08-23 22:48:00 -0500

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
-    name            =   'admin_bootstrap',
-    version         =   '0.5',
-    description     =   'Django admin templates that are compatible with Twitter Bootstrap version 2.',
-    author          =   'Daniel Ryan',
-    author_email    =   'dryan@dryan.com',
-    url             =   'https://github.com/ofa/admin_bootstrap',
-    packages        =   ['admin_bootstrap'],
+    name                    =   'admin_bootstrap',
+    version                 =   '0.6.0',
+    description             =   'Django admin templates that are compatible with Twitter Bootstrap version 2.',
+    author                  =   'Daniel Ryan',
+    author_email            =   'dryan@dryan.com',
+    url                     =   'https://github.com/dryan/admin_bootstrap',
+    packages                =   find_packages(),
+    long_description        =   open('README', 'r').read(),
+    license                 =   'BSD 3 Clause License',
+    zip_safe                =   False,
+    include_package_data    =   True,
 )
